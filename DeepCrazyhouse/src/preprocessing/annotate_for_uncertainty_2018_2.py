b150b225b90876786e8fa366f32658fdb26f36fd
Martin Ruzicka <martin.ruzicka@stud.tu-darmstadt.de>
2023-03-23 21:22:10 +0100

    june16

--- DeepCrazyhouse/src/preprocessing/annotate_for_uncertainty_2018_2.py
+++ DeepCrazyhouse/src/preprocessing/annotate_for_uncertainty_2018_2.py
@@ -395,30 +395,18 @@ def change_binary_name(binary_dir: str, current_binary_name: str, process_name: 
 
 if __name__ == "__main__":
     dummy = []
-    for month in range(7, 10):
-        dummy.extend(glob.glob(
-            '/home/ml-mruzicka/failed/*2018-0{}*.zip'.format(
-                month)))
-    for month in range(10, 13):
-        dummy.extend(glob.glob(
-            '/home/ml-mruzicka/failed/*2018-{}*.zip'.format(
-                month)))
+    dummy.extend(glob.glob(
+        '/home/ml-mruzicka/failed/*2016-06*.zip'))
     max_iter = len(dummy)
-    rtpt = RTPT(name_initials='MR', experiment_name='AnnotateCrazyhouse_18_rest', max_iterations=max_iter)
+    rtpt = RTPT(name_initials='MR', experiment_name='AnnotateCrazyhouse_16_6', max_iterations=max_iter)
     rtpt.start()
     current_binary_name = 'CrazyAra'
     dataset_types = ["train"]#, "val", "test", "mate_in_one"]
     for dataset_type in dataset_types:
         zarr_filepaths = []
         if dataset_type == "train":
-            for month in range(7, 10):
-                zarr_filepaths.extend(glob.glob(
-                    '/home/ml-mruzicka/failed/*2018-0{}*.zip'.format(
-                        month)))
-            for month in range(10, 13):
-                zarr_filepaths.extend(glob.glob(
-                    '/home/ml-mruzicka/failed/*2018-{}*.zip'.format(
-                        month)))
+            zarr_filepaths.extend(glob.glob(
+                '/home/ml-mruzicka/failed/*2016-06*.zip'))
         elif dataset_type == "val":
             zarr_filepaths = glob.glob(main_config["planes_val_dir"] + "**/*.zip")
         elif dataset_type == "test":
